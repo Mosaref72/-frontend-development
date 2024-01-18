aef389cba9971fc5ac70a954eebbd23af8373f88
Mosaref72 <mosarefpaik.in@gmail.com>
2024-01-18 12:43:48 +0530

    js  file add

---
+++ javascript/01-basic/script.js
@@ -0,0 +1,18 @@
+let myobj = {
+    userName: "Mosaref",
+    age: 23,
+    crouse: "javascript",
+    price: 2999
+}
+
+// console.log(myobj.userName);
+// console.log(myobj.price);
+// Destructuring
+
+const {userName,age,price} =myobj;
+
+// console.log(`My name is ${myobj.userName} age is ${myobj.age}`);
+
+console.log(`my name is ${userName} age is ${age}`);
+
+
