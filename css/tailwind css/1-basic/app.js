9b2bf03887f79960599f590b9978a1155103c5f3
Mosaref72 <mosarefpaik.in@gmail.com>
2024-01-20 17:53:39 +0530

    tailwind css setup

---
+++ css/tailwind css/1-basic/app.js
@@ -0,0 +1,53 @@
+// console.log('1'+2+3);
+
+// console.log([1,2,3].map(parseInt)); // output [1,NaN,NaN]
+
+
+// let x = 2022;
+// function year() {
+//    var x = 2021;
+//    return this.x;
+// }
+// year();
+// console.log(x);
+
+// console.log([3,2,1].reduce(Math.pow));
+
+// let x = 10;
+
+// function myFunction(){
+//      x += 5; console.log(x); }
+//  myFunction(); 
+ 
+// console.log(x);
+
+// let y = 20;
+// function myfun() {
+//     y += 15;
+//     console.log(y);
+// }
+// myfun()
+// console.log(y);
+
+
+// console.log(typeof NaN)
+
+// console.log(typeof null);
+
+// let a = 10;
+// let b = '5';
+// console.log(a+b);
+
+
+// console.log(Boolean(null));
+
+// console.log(typeof typeof 1);
+
+// let a = [1, 2, 3]; 
+// for (var i = 0; i <= a.length; i++) {console.log(a[i]);}
+
+// var x = 30; function myFunc() { var x = 20; return x; } console.log(x); console.log(myFunc());
+let a = [1, 2, 3];
+let b = a;
+b.push(4);
+console.log(a);
---
+++ css/tailwind css/1-basic/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["*.html"];
+export const theme = {
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg:"976px",
+    xl:"1440px"
+  },
+  extend: {},
+};
+export const plugins = [];
+
