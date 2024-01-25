2f22669fe6b89a1aa3f167886b5b7665a08f834e
Mosaref72 <mosarefpaik.in@gmail.com>
2024-01-25 23:21:27 +0530

    html h3 random password show

--- javascript/01-basic/app.js
+++ javascript/01-basic/app.js
@@ -1,16 +1,17 @@
-function passwordgen(passwordlantgh) {
+function passwordgen() {
     let char = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbn!@#$%^&*(){}[]"
     let password = " ";
+    let passwordlantgh = 12
     
     for (let i = 0; i < passwordlantgh; i++) {
         let randompass = Math.floor(Math.random() * char.length)
         password += char.charAt(randompass)
         // anathor type
         // password += char[randompass]
+        document.querySelector('h3').innerHTML = password
         
     }
     return password;
 }
-let password = passwordgen(12)
-console.log(password);
+
 
