b183fe9c9e2161223a78e8ea68fa475a0755ae90
Mosaref72 <mosarefpaik.in@gmail.com>
2024-01-31 10:25:09 +0530

    add new js

---
+++ javascript/01-basic/main.js
@@ -0,0 +1,9 @@
+function myfun(num1,num2) {
+    let result = num1 + num2 ;
+    return result;
+}
+
+console.log(myfun(4,5));
+
+
+
--- javascript/01-basic/script.js
+++ javascript/01-basic/script.js
@@ -15,15 +15,39 @@
 
 // console.log(`my name is ${userName} age is ${age}`);
 
-class myfam{
-    constructor(names,age){
-        names = this.names,
-        age = this.age
-        
-    }
 
+// Define a class called 'Person'
+class Person {
+    // Constructor method to initialize properties
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    // Method to greet
+    greet() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+  
+  // Create an instance of the Person class
+  let person1 = new Person("John", 30);
+  
+  // Call the greet method
+  person1.greet(); // Output: Hello, my name is John and I am 30 years old.
+
+
+class Taskin{
+  constructor(arg1,arg2) {
+    this.arg1 = arg1;
+    this.arg2 = arg2;
+  }
+  bilkis() {
+    console.log(`My name is ${this.arg1} age is ${this.arg2}`);
+  }
 }
 
+let mpaik = new Taskin("Mosaref paik" , 23)
 
-
-// console.log(fName("mosaref",28));
+mpaik.bilkis()
+  
